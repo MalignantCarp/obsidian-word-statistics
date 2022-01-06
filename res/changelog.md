07a7915caba7fccde444bae38f50d4201a251c80
MalignantCarp <93364935+MalignantCarp@users.noreply.github.com>
2022-01-05 22:45:40 -0700

    2022-01-05
     - Added some more regex work mostly. Will likely start working on the main word counting stuff tomorrow and will handle embeds afterwards.

--- res/changelog.md
+++ res/changelog.md
@@ -1,2 +1,5 @@
+2022-01-05
+ - Added some more regex work mostly. Will likely start working on the main word counting stuff tomorrow and will handle embeds afterwards.
+
 2022-01-04
  - Added words.ts with the first of our functions. Should hopefully have basic word counting of Markdown content by the weekend, hopefully in the status bar.
--- src/words.ts
+++ src/words.ts
@@ -10,6 +10,10 @@ const RE_HTML_COMMENT = /<!--.*?-->/sg;
 const RE_MD_COMMENT = /%%.*?%%/sg;
 const RE_FOOTNOTE = /\[^[\P{L}\P{N}_]+\]/gum;
 const RE_TASK_START = /(^\s*-\s\[\S\]\s)/gmu;
+const RE_EMBED = /(?:!\[\[)(.*?)(?:\]\])/gm;
+const RE_EMBED_CONTENT = /(?<=!\[\[)(.*?)(?=\]\])/gm;
+const RE_HTML_EMBED = /<iframe.*?<\/iframe>/gms;
+const RE_HTML_TAG = /(?<!\\)\<\/?.*?(?!\\)>/gms;
 
 const CountWords = (text: string): number => {
     let words = 0;
@@ -32,19 +36,47 @@ const SanitizeText = (text: string): string => {
 
     // we now need to match [[link|alt]] and [alt](link) bits and replace them with alt
     console.log(text);
-    text.replace(RE_WIKILINK, "$2") // [[link|alt text]]
+    text.replace(RE_WIKILINK, "$2"); // [[link|alt text]]
     console.log("-->", text);
-    text.replace(RE_MDLINK, "$2") // [alt text](link)
+    text.replace(RE_MDLINK, "$2"); // [alt text](link)
     console.log("-->", text);
     // remove anything inside a ` - [\S]` bit since we don't want the [x] in a checkbox to register as a word
     text.replace(RE_TASK_START, "");
 
+    // remove HTML embeds
+    text.replace(RE_HTML_EMBED, "");
+
+    // we should probably attempt to identify other HTML tags and remove the tags themselves, leaving any text content
+    // since we've already wiped out comments, this shouldn't be problematic.
+    text.replace(RE_HTML_TAG, "");
+
+    // remove markdown embeds
+    text.replace(RE_EMBED, ""); // these will have already been collected to be catalogued as required
+
+    // we can probably ignore whatever is in code blocks as well, with the exception of admonitions. Need to examine all the usecases.
+    // Probably can ignore the first line with the backticks
+
     return text;
 };
 
-const CollectText = (text: string): string[] => {
+const CollectEmbeds = (text: string): string[] => {
+    let embeds: string[] = [];
+
+    embeds = ((text || '').match(RE_EMBED_CONTENT) || []);
+    // we need to clear image and other non-vault entities out
+
+    // we need to extract the content of the embeds by calling up the links making sure to not infinitely recurse
+    // may be worth looking at Quoth to possibly support its embeds as well
+    // remember that if there are any #headings referenced in the embed declaration, we need to only include what is bound to that
+    // heading
+
+    return embeds;
+};
+
+const CollectText = (text: string): [string, string[]] => {
     // this function will return the main text as the first element, after sanitizing,
     // and then any subsequent blocks from transclusions
-    let textBlocks: string[] = [];
-    return textBlocks;
+    let embedContent = CollectEmbeds(text);
+    let wordText = SanitizeText(text);
+    return [wordText, embedContent];
 };
